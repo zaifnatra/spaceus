d27a3bdffffcf07fb805206b3ac39b5596b786d7
saymonnsays <saysaycruz@yahoo.com>
2023-11-05 13:42:01 -0500

    fixed font colors and everyhting
    aproved by zaif

--- sprite final backup/main/launcher.java
+++ sprite final backup/main/launcher.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.Color;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -29,21 +30,27 @@ public class launcher {
                 g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
             }
         };
-        panel.setPreferredSize(new Dimension(600, 400));
+        panel.setPreferredSize(new Dimension(800, 600)); // Increase the panel size
+
+        // Use a pixel art font (replace "PixelFont.ttf" with the actual font file)
+        Font pixelFont = loadPixelArtFont("/res/space_background_pack/layers/04B_30__.TTF");
+
         JLabel titleLabel = new JLabel("Welcome to Space Beyond Us!");
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
-        c.insets = new Insets(0, 0, 0, 0);
+        c.insets = new Insets(50, 0, 20, 0); // Increased top and bottom insets
         panel.add(titleLabel, c);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        titleLabel.setFont(pixelFont.deriveFont(Font.BOLD, 30));
+        titleLabel.setForeground(Color.WHITE); // Increased font size
 
         JLabel beginLabel = new JLabel("Begin?");
         c.gridx = 0;
         c.gridy = 1;
-        c.insets = new Insets(0, 0, 0, 0);
+        c.insets = new Insets(20, 0, 20, 0); // Increased top and bottom insets
         panel.add(beginLabel, c);
-        beginLabel.setFont(titleLabel.getFont()); // reuse titleLabel's font
+        beginLabel.setFont(pixelFont.deriveFont(20)); // Increased font size
+
         JButton startButton = new JButton("Start Game");
         startButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -52,9 +59,11 @@ public class launcher {
             }
         });
         c.gridx = 0;
-        c.gridy = 1;
-        c.insets = new Insets(50, 0, 0, 400);
+        c.gridy = 2;
+        c.insets = new Insets(50, 0, 20, 400); // Increased top and bottom insets, adjusted right inset
         panel.add(startButton, c);
+        startButton.setFont(pixelFont.deriveFont(Font.BOLD, 12)); // Increased font size for buttons
+        startButton.setPreferredSize(new Dimension(200, 60)); // Set preferred button size
 
         JButton quitButton = new JButton("I am a coward");
         quitButton.addActionListener(new ActionListener() {
@@ -63,12 +72,23 @@ public class launcher {
             }
         });
         c.gridx = 1;
-        c.insets = new Insets(50, -200, 0, 0);
+        c.insets = new Insets(50, -200, 20, 0); // Increased top and bottom insets, adjusted left inset
         panel.add(quitButton, c);
+        quitButton.setFont(pixelFont.deriveFont(Font.BOLD, 12)); // Increased font size for buttons
+        quitButton.setPreferredSize(new Dimension(200, 60)); // Set preferred button size
 
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static Font loadPixelArtFont(String fontPath) {
+        try {
+            return Font.createFont(Font.TRUETYPE_FONT, launcher.class.getResourceAsStream(fontPath));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Font("Arial", Font.PLAIN, 12); // Default font if the pixel art font fails to load
+        }
     }
 }
